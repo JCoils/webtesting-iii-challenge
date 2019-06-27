59d3c3906452007efa10efee970d650da6daefc3
JordanG <jmg236@yahoo.com>
2019-06-27 15:23:12 -0500

    Dash

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,3 +1,33 @@
 // Test away
 
 
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import Dashboard from './Dashboard'
+
+
+afterEach(cleanup)
+
+
+describe('Dashboard.js', () => {
+  test('it renders without errors', () => {
+    render(<Dashboard />)
+  })
+
+  test('it shows the controls and display', () => {
+    const { container } = render(<Dashboard />)
+    const controls = container.getElementsByClassName('controls')
+    const display = container.getElementsByClassName('display')
+
+    expect(controls.length).toBe(1)
+    expect(display.length).toBe(1)
+  })
+
+  describe('<Display />', () => {
+    test('it defaults to `unlocked` and `open`', () => {
+      const { container, getByText } = render(<Dashboard />)
+      getByText(/Unlocked/)
+      getByText(/Open/)
+    })
+  })
+})
